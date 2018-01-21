bd8422ca569d0e7a44fd77fbd874059af40686e5
Shubham <syadav.yadav32@gmail.com>
2018-01-21 05:55:00 +0000

    Instagram integration completed

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,8 @@ class TweetsController < ApplicationController
   def index
     @hashtag_id = params[:id]
     @hashtag = Hashtag.find(@hashtag_id).htname
-    @htname = @hashtag.prepend("#")
     @hashtag_posts = InstagramApi.tag(@hashtag).recent_media({count:10}).data
+    @htname = @hashtag.prepend("#")
+    
   end
 end
