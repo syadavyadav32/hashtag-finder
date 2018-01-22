180d6ea5fbdf6bad26ef88c0bef1e0b40557c346
Shubham <syadav.yadav32@gmail.com>
2018-01-22 06:13:44 +0000

    Google maps integration

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'twitter'
 gem 'instagram_api_client'
+gem 'gmaps4rails'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,18 @@
 class TweetsController < ApplicationController
-  def index
+  def index 
     @hashtag_id = params[:id]
     @hashtag = Hashtag.find(@hashtag_id).htname
     @hashtag_posts = InstagramApi.tag(@hashtag).recent_media({count:10}).data
     @htname = @hashtag.prepend("#")
     
+    @hashtag_posts.each do |htp|
+      @hash = Gmaps4rails.build_markers (htp) do |hashtag, marker|
+        if hashtag.location
+          marker.lat hashtag.location.latitude
+          marker.lng hashtag.location.longitude
+          marker.infowindow hashtag.caption.text
+        end
+      end
+    end
   end
 end
